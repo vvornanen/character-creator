2dc3e022ad7e7cae3472ee7f361d7bf592cbecab
Ville Vornanen <8843354+vvornanen@users.noreply.github.com>
2024-04-23 22:55:10 +0300

    Add mobile variant

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -32,6 +32,17 @@ function App() {
       <main className={styles.characterEditor}>
         <div className={styles.perspectiveDecorator}/>
 
+        <div className={styles.characterWrapper}>
+          <Character
+              body={body}
+              head={head}
+              face={face}
+              accessory={accessory}
+              skinColor={skinColor}
+              clothesColor={clothesColor}
+          />
+        </div>
+
         <MaxWidthWrapper className={styles.maxWidthWrapper}>
           <header className={styles.header}>
             <h1 className={styles.title}>Create your Character</h1>
@@ -79,17 +90,6 @@ function App() {
             />
           </div>
         </MaxWidthWrapper>
-
-        <div className={styles.characterWrapper}>
-          <Character
-              body={body}
-              head={head}
-              face={face}
-              accessory={accessory}
-              skinColor={skinColor}
-              clothesColor={clothesColor}
-          />
-        </div>
       </main>
   );
 }
